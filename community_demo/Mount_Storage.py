95488e1c06b0933f9c9fed71653394b1fc3b5c54
Mark Salama <mark.salama@pwc.com>
2025-02-23 02:31:33 +0200

    first_deploy_bundle

--- community_demo/Mount_Storage.py
+++ community_demo/Mount_Storage.py
@@ -5,9 +5,9 @@ mount_point = dbutils.widgets.get("mount_point")
 configs = {
     "fs.azure.account.auth.type": "OAuth",
     "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-    "fs.azure.account.oauth2.client.id": "fba6beb7-2b89-46dd-95a5-f30505b8ffc2",
-    "fs.azure.account.oauth2.client.secret": "vkl8Q~46SI4iI2AQ3--Seeler81QSE0vcRbm5cYH",
-    "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/<tenant_id>/oauth2/token"
+    "fs.azure.account.oauth2.client.id": dbutils.secrets.get(scope="dbw-comm-scope", key="sp_accessdata_id"),
+    "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="dbw-comm-scope", key="sp_accessdata_secret"),
+    "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{dbutils.secrets.get(scope='dbw-comm-scope', key='tenant_id')}/oauth2/token"
 }
 
 dbutils.fs.mount(
